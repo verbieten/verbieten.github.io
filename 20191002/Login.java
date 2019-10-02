d9839b5c3ffd8346179fbf252c46734b04e906c0
verbieten <37131279+verbieten@users.noreply.github.com>
2019-10-02 19:06:23 +0900

    Create Login.java

---
+++ 20191002/Login.java
@@ -0,0 +1,14 @@
+package bean.user;
+
+public class Login {
+
+	private String userName;
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+}
