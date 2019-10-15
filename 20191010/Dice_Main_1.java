b7ede6bce12fea870d4503892d701a9cb004dc62
verbieten <37131279+verbieten@users.noreply.github.com>
2019-10-15 11:53:07 +0900

    Create Dice_Main_1.java

---
+++ 20191010/Dice_Main_1.java
@@ -0,0 +1,10 @@
+package bean.user;
+import bean.user.*;
+public class Dice_Main_1 {
+	public static void main(String [] args) {
+		bean.user.Dice dice = new bean.user.Dice();
+		System.out.println("주사위 2번 던지기");
+		System.out.println("주사위 1 : " + dice.getNum());
+		System.out.println("주사위 2 : " + dice.getNum());
+		}
+}
