636a5908fb4ff9257b5060c4f651799254bb6280
verbieten <37131279+verbieten@users.noreply.github.com>
2019-11-14 12:03:28 +0900

    Create Userinfo.java

---
+++ 20191114/Userinfo.java
@@ -0,0 +1,34 @@
+package bean.user;
+public class Userinfo {
+	private String stud_id, stud_passwd, stud_name, stud_email, stud_gender;
+	public String getStud_id(){
+		return stud_id;
+	}
+	public void setStud_id(String stud_id){
+		this.stud_id = stud_id;
+	}
+	public String getStud_passwd(){
+		return stud_passwd;
+	}
+	public void setStud_passwd(String stud_passwd){
+		this.stud_passwd = stud_passwd;
+	}
+	public String getStud_name(){
+		return stud_name;
+	}
+	public void setStud_name(String stud_name){
+		this.stud_name = stud_name;
+	}
+	public String getStud_email(){
+		return stud_email;
+	}
+	public void setStud_email(String stud_email){
+		this.stud_email = stud_email;
+	}
+	public String getStud_gender(){
+		return stud_gender;
+	}
+	public void setStud_gender(String stud_gender){
+		this.stud_gender = stud_gender;
+	}
+}
