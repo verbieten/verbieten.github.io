26ff1cfaa5781e90131e84a8e77a5e14fad026b3
verbieten <37131279+verbieten@users.noreply.github.com>
2019-11-19 21:39:32 +0900

    Create Bookinfo.java

---
+++ 20191114/Bookinfo.java
@@ -0,0 +1,35 @@
+package bean.user;
+public class Bookinfo {
+	private String book_name, book_author, book_publish, book_genre, book_price;
+	public String getBook_name(){
+		return book_name;
+	}
+	public void setBook_name(String book_name){
+		this.book_name = book_name;
+	}
+	public String getBook_author(){
+		return book_author;
+	}
+	public void setBook_author(String book_author){
+		this.book_author = book_author;
+	}
+	public String getBook_publish(){
+		return book_publish;
+	}
+	public void setBook_publish(String book_publish){
+		this.book_publish = book_publish;
+	}
+	public String getBook_genre(){
+		return book_genre;
+	}
+	public void setBook_genre(String book_genre){
+		this.book_genre = book_genre;
+	}
+	public String getBook_price(){
+		return book_price;
+	}
+	public void setBook_price(String book_price){
+		this.book_price = book_price;
+	}
+}
+
