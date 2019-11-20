5fb572fc17b39330723419b1a2831ad4f421b52f
verbieten <37131279+verbieten@users.noreply.github.com>
2019-11-20 18:59:48 +0900

    Create StuduserDTO.java

---
+++ 20191120/StuduserDTO.java
@@ -0,0 +1,73 @@
+package studuserpkg;
+public class StuduserDTO {
+	String stud_id;
+	public String getStud_id(){
+		return stud_id;
+	}
+	public void setStud_id(String stud_id){
+		this.stud_id=stud_id;
+	}
+	String stud_passwd;
+	public String getStud_passwd(){
+		return stud_passwd;
+	}
+	public void setStud_passwd(String stud_passwd) {
+		this.stud_passwd = stud_passwd;
+	}
+	String stud_name;
+	public String getStud_name(){
+		return stud_name;
+	}
+	public void setStud_name(String stud_name) {
+		this.stud_name = stud_name;
+	}
+	String stud_email;
+	public String getStud_email(){
+		return stud_email;
+	}
+	public void setStud_email(String stud_email) {
+		this.stud_email = stud_email;
+	}
+	String stud_phone;
+	public String getStud_phone(){
+		return stud_phone;
+	}
+	public void setStud_phone(String stud_phone) {
+		this.stud_phone = stud_phone;
+	}
+	String stud_gender;
+	public String getStud_gender(){
+		return stud_gender;
+	}
+	public void setStud_gender(String stud_gender) {
+		this.stud_gender = stud_gender;
+	}
+	String stud_hobby1;
+	public String getStud_hobby1(){
+		return stud_hobby1;
+	}
+	public void setStud_hobby1(String stud_hobby1) {
+		this.stud_hobby1 = stud_hobby1;
+	}
+	String stud_hobby2;
+	public String getStud_hobby2(){
+		return stud_hobby2;
+	}
+	public void setStud_hobby2(String stud_hobby2) {
+		this.stud_hobby2 = stud_hobby2;
+	}
+	String stud_hobby3;
+	public String getStud_hobby3(){
+		return stud_hobby3;
+	}
+	public void setStud_hobby3(String stud_hobby3) {
+		this.stud_hobby3 = stud_hobby3;
+	}
+	String stud_regdate;
+	public String getStud_regdate(){
+		return stud_regdate;
+	}
+	public void setStud_regdate(String stud_regdate) {
+		this.stud_regdate = stud_regdate;
+	}
+}
