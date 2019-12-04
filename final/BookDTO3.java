3556f5678805cf9f520fd1dab3a3cbf3fe530d16
verbieten <37131279+verbieten@users.noreply.github.com>
2019-12-04 20:37:24 +0900

    Create BookDTO3.java

---
+++ final/BookDTO3.java
@@ -0,0 +1,22 @@
+package book2pkg;
+public class BookDTO3
+{
+	String book_name;
+	public String getBook_name(){return book_name;}
+	public void setBook_name(String book_name){ this.book_name=book_name;}
+	String book_author;
+	public String getBook_author(){return book_author;}
+	public void setBook_author(String book_author){ this.book_author=book_author;}
+	String book_publish;
+	public String getBook_publish(){return book_publish;}
+	public void setBook_publish(String book_publish){ this.book_publish=book_publish;}
+	String book_price;
+	public String getBook_price(){return book_price;}
+	public void setBook_price(String book_price){ this.book_price=book_price;}
+	String book_isbn;
+	public String getBook_isbn(){return book_isbn;}
+	public void setBook_isbn(String book_isbn){ this.book_isbn=book_isbn;}
+	String book_genre;
+	public String getBook_genre(){return book_genre;}
+	public void setBook_genre(String book_genre){ this.book_genre=book_genre;}
+}
